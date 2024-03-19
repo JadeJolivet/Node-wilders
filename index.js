dcc216656bb50f2ae0f3c202a1d550f1d600d34b
Jade Jolivet <jade.jolivet@gmail.com>
2024-03-19 11:06:39 +0100

    repo node

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello Sylvain!'));
+console.log(chalk.red('Hello Sacha!'));
+console.log(chalk.green('Hello Alice!'));
+console.log(chalk.yellow('Hello PJ!'));
